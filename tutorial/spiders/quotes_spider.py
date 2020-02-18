23550cc5538a5d97ca2a22b954f33192f8ce42fb
Cassidy Dobratz <cdobratz@protonmail.com>
2020-02-17 22:32:24 -0700

    new json file

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -6,6 +6,14 @@ class QuotesSpider(scrapy.Spider):
     start_urls = [
         'http://quotes.toscrape.com/page/1/',
         'http://quotes.toscrape.com/page/2/',
+        'http://quotes.toscrape.com/page/3/',
+        'http://quotes.toscrape.com/page/4/',
+        'http://quotes.toscrape.com/page/5/',
+        'http://quotes.toscrape.com/page/6/',
+        'http://quotes.toscrape.com/page/7/',
+        'http://quotes.toscrape.com/page/8/',
+        'http://quotes.toscrape.com/page/9/',
+        'http://quotes.toscrape.com/page/10/',
     ]
 
 
